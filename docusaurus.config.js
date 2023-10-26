6fd01222368a5ddb7e8e8cdffb8da8df614efbf1
AnthonyCvn <anthony.cavin.ac@gmail.com>
2023-10-26 14:50:30 +0200

    Initial commit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,7 +19,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "reductstore", // Usually your GitHub org/user name.
-  projectName: "reductstore", // Usually your repo name.
+  projectName: "docs", // Usually your repo name.
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
