1642c17b9cebbfeb3b20e024b74e85597e2fa265
Alexey Timin <atimin@gmail.com>
2023-12-31 11:53:00 +0100

    www.reduct.store

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://reduct.store",
+  url: "https://www.reduct.store",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
