1e94bc115754e490eec56d852ba3237f4c677eab
Anthony <anthony.cavin.ac@gmail.com>
2024-01-10 18:04:23 +0100

    Rs 98 configure matomo for gdpr compliance (#6)
    * Remove umami analytics
    
    * Update Privacy to comply with Matamo
    
    * Wrap MatomoOptOut in a React component
    
    * Add margin arround Matomo opt-out
    
    * Add Matomo link
    
    * Fix bug when removing Matomo opt-out script

---
+++ src/components/MatomoOptOut/index.tsx
@@ -0,0 +1,25 @@
+import React, { useEffect } from 'react';
+
+const MatomoOptOut = () => {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = "https://reductstore.matomo.cloud/index.php?module=CoreAdminHome&action=optOutJS&divId=matomo-opt-out&language=auto&showIntro=0";
+    script.async = true;
+
+    const matomoElement = document.getElementById('matomo-opt-out');
+    if (matomoElement) {
+      matomoElement.appendChild(script);
+    }
+
+    return () => {
+      // Cleanup the script when the component is unmounted
+      if (matomoElement && matomoElement.contains(script)) {
+        matomoElement.removeChild(script);
+      }
+    };
+  }, []);
+
+  return <div id="matomo-opt-out" style={{ marginBottom: '20px' }} />;
+};
+
+export default MatomoOptOut;
