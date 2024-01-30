1eb49b208e394fb59c606946fa2f7fd04376188b
Alexey Timin <atimin@gmail.com>
2024-01-30 09:53:55 +0100

    RS-105: Release 1.8.0 (#14)
    * add release note without images
    
    * add images
    
    * update version and release documentation
    
    * last version is 1.8.1
    
    * links in bold
    
    * improve after review
    
    * title
    
    * replicated title
    
    * bold links
    
    ---------
    
    Co-authored-by: AnthonyCvn <anthony.cavin.ac@gmail.com>

--- src/pages/download/index.tsx
+++ src/pages/download/index.tsx
@@ -16,7 +16,7 @@ export default function DownloadPage(): JSX.Element {
       <main>
         <SimpleHeader pageTitle="Download ReductStore" />
         <div className="container">
-          <InstallReductStore />
+          <InstallReductStore version="v1.8.1" />
           <hr />
           <ClientSDKs />
           <hr />
