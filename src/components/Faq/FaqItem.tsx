66659299737ac131221b5616d26e2e7cd8846221
Anthony <anthony.cavin.ac@gmail.com>
2024-02-09 16:12:41 +0100

    RS-103: New pricing offer (#16)
    * new pricing offer
    
    * text corrections

---
+++ src/components/Faq/FaqItem.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from "./styles.module.css";
+import { FaChevronDown, FaChevronRight } from 'react-icons/fa';
+
+const FaqItem = ({ question, answer, isOpen, toggle }) => {
+  const chevronSize = "0.8em";
+  return (
+    <div className={styles.faqItem}>
+      <div className={styles.faqQuestion} onClick={toggle}>
+        <div className={styles.faqIcon}>
+          {isOpen ? <FaChevronDown size={chevronSize} /> : <FaChevronRight size={chevronSize} />}
+        </div>
+        {question}
+      </div>
+      {isOpen && <div className={styles.faqAnswer}>{answer}</div>}
+    </div>
+  );
+};
+
+export default FaqItem;
