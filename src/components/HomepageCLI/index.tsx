fa5d8ae823cfa8b8476dd79723019a9433475423
AnthonyCvn <anthony.cavin.ac@gmail.com>
2024-04-08 22:44:04 +0200

    Show terminal image on Mobile and add responsive step

--- src/components/HomepageCLI/index.tsx
+++ src/components/HomepageCLI/index.tsx
@@ -1,22 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import clsx from "clsx";
 import styles from "./styles.module.css";
 import Link from "@docusaurus/Link";
 import BrowserOnly from "@docusaurus/BrowserOnly";
 
+const CliDemoImg = require("@site/static/img/cli-demo.webp").default;
+
 function HomepageCLI() {
+  const [isLargeScreen, setIsLargeScreen] = useState(window.innerWidth >= 1200); // Example breakpoint
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsLargeScreen(window.innerWidth >= 1120);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
     <div className={styles.cliSection}>
       <div className="row" style={{ display: 'flex', alignItems: 'center' }}>
-        <div className={clsx("col col--7", styles.cliAnimation)}>
-          <BrowserOnly>
-            {() => {
-              const TerminalAnimation = require('../TerminalAnimation').default;
-              return <TerminalAnimation />;
-            }}
-          </BrowserOnly>
+        <div className={clsx(isLargeScreen ? "col col--8" : "col col--9", styles.cliAnimation)}>
+          <div className="hideOnMobile" style={{ width: '100%' }} >
+            <BrowserOnly>
+              {() => {
+                const TerminalAnimation = require('../TerminalAnimation').default;
+                return <TerminalAnimation />;
+              }}
+            </BrowserOnly>
+          </div>
+          <div className={clsx("showOnMobile", styles.mobileImage)}>
+            <img src={CliDemoImg} alt="Mobile View" style={{ maxWidth: '100%', height: 'auto' }} />
+          </div>
         </div>
-        <div className={"col col--5 text--center"}>
+        <div className={clsx(isLargeScreen ? "col col-4" : "col col--3", "text--center")}>
           <h2 className="hideOnMobile">CLI Client</h2>
           <p>
             You can customize data retention and replication policies using the{" "}
