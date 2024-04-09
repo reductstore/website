fd18aeeac45fea2b7c67e3a78b2e90f04f44c37f
Alexey Timin <atimin@gmail.com>
2024-04-09 19:53:04 +0200

    RS-213: Rust ReductCLI (#30)
    * change download page for Rust CLI
    
    * add artilce about reduct cli migration
    
    * terminal animation
    
    * only import terminal animation client-side
    
    * missing sudo
    
    * add link and improve terminal animation
    
    * center animation in a flex way and hide verflow
    
    * update rust cli link and description
    
    * update reduct cli link
    
    * Show terminal image on Mobile and add responsive step
    
    * align items and get smae style between terminal and web console images
    
    * fix issue server side window function
    
    ---------
    
    Co-authored-by: AnthonyCvn <anthony.cavin.ac@gmail.com>

--- src/components/HomepageCLI/index.tsx
+++ src/components/HomepageCLI/index.tsx
@@ -1,22 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import clsx from "clsx";
 import styles from "./styles.module.css";
-import CodeBlock from "@theme/CodeBlock";
 import Link from "@docusaurus/Link";
+import BrowserOnly from "@docusaurus/BrowserOnly";
+
+const CliDemoImg = require("@site/static/img/cli-demo.webp").default;
 
 function HomepageCLI() {
+  const [isLargeScreen, setIsLargeScreen] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsLargeScreen(window.innerWidth >= 1120);
+    };
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
     <div className={styles.cliSection}>
       <div className="row" style={{ display: 'flex', alignItems: 'center' }}>
-        <div className={clsx("col col--7", styles.cliImage)}>
-          <CodeBlock className="language-bash">{resonseCLI}</CodeBlock>
+        <div className={clsx(isLargeScreen ? "col col--8" : "col col--9", styles.cliAnimation)}>
+          <div className="hideOnMobile" style={{ width: '100%' }} >
+            <BrowserOnly>
+              {() => {
+                const TerminalAnimation = require('../TerminalAnimation').default;
+                return <TerminalAnimation />;
+              }}
+            </BrowserOnly>
+          </div>
+          <div className={clsx("showOnMobile", styles.mobileImage)}>
+            <img src={CliDemoImg} alt="Mobile View" style={{ maxWidth: '100%', height: 'auto' }} />
+          </div>
         </div>
-        <div className={"col col--5 text--center"}>
+        <div className={clsx(isLargeScreen ? "col col-4" : "col col--3", "text--center")}>
           <h2 className="hideOnMobile">CLI Client</h2>
           <p>
             You can customize data retention and replication policies using the{" "}
             <strong>
-              <Link to="https://cli.reduct.store">
+              <Link to="https://github.com/reductstore/reduct-cli">
                 ReducStore CLI client
               </Link>
             </strong>.
@@ -26,30 +49,5 @@ function HomepageCLI() {
     </div>
   );
 }
-
-const resonseCLI = `
-reduct@store % pip install reduct-cli
-reduct@store % rcli alias  add -L  https://play.reduct.store -t reduct play
-reduct@store % rcli bucket show play/data --full
-╭───────────────────── Info ─────────────────╮╭───────── Settings ─────────╮
-│ Entry count:         1                     ││ Quota Type:         FIFO   │
-│ Size:                30 GB                 ││ Quota Size:         30 GB  │
-│ Oldest Record (UTC): 2022-12-20T16:36:41   ││ Max. Block Size:    64 MB  │
-│ Latest Record (UTC): 2022-12-20T19:15:26   ││ Max. Block Records: 1024   │
-│ History Interval:    3 hour(s)             ││                            │
-│                                            ││                            │
-│                                            ││                            │
-│                                            ││                            │
-│                                            ││                            │
-│                                            ││                            │
-╰────────────────────────────────────────────╯╰────────────────────────────╯
-Entries
-┏━━━━━━┳━━━━━━━━━┳━━━━━━━┳━━━━━━━━━━━━━━━┳━━━━━━━━━━━━━━━━┳━━━━━━━━━━━┓
-┃      ┃         ┃       ┃ Oldest Record ┃ Latest Record  ┃           ┃
-┃ Name ┃ Records ┃ Size  ┃ (UTC)         ┃ (UTC)          ┃ History   ┃
-┡━━━━━━╇━━━━━━━━━╇━━━━━━━╇━━━━━━━━━━━━━━━╇━━━━━━━━━━━━━━━━╇━━━━━━━━━━━┩
-│ test │ 57154   │ 30 GB │ 2022-12-20T1… │ 2022-12-20T19… │ 3 hour(s) │
-└──────┴─────────┴───────┴───────────────┴────────────────┴───────────┘
-`.trim();
 
 export default HomepageCLI;
