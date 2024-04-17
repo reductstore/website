b6c74639f9e80d27b76e9496abdac792aa07ad81
Alexey Timin <atimin@gmail.com>
2024-04-17 22:47:55 +0200

    RS-208: Buckets Guide (#33)
    * draft for bucket documentation
    
    * add snippets for creating a bucket
    
    * test python examples in pipeline
    
    * fix python instllaiton step
    
    * remove stable version
    
    * fix path to examples
    
    * fix path
    
    * update action
    
    * add canonical links
    
    * improve examples running
    
    * test node and python example together
    
    * test
    
    * add package.js
    
    * test curl
    
    * fix path for curl
    
    * fix curl example
    
    * fix cpp example
    
    * update actions for cpp
    
    * fix many errors
    
    * fix cpp build
    
    * fix cpp paths
    
    * install reduct-cpp
    
    * sudo make install
    
    * fix path after reduct-cpp
    
    * clean build dir
    
    * try to fix cpp call
    
    * fix CMakeLists
    
    * fix cpp run
    
    * fix path for find
    
    * fix again
    
    * fix find command
    
    * fix find cmd
    
    * check
    
    * debug
    
    * rename examples
    
    * test docker-compose
    
    * test rust example
    
    * fix api-token
    
    * fix rust example
    
    * use -maxdepth
    
    * fix rust paths
    
    * fix rust path
    
    * remove find
    
    * fix path
    
    * fix cpp example name
    
    * fix rust paths
    
    * fix cpp
    
    * finish creating bucket section
    
    * add cli examples
    
    * fix cli cmd
    
    * refactor ci
    
    * fix browse cli example
    
    * add sleep to run database
    
    * add python example
    
    * add browsing bucket section
    
    * copy some data
    
    * fix bucket name
    
    * re run cpp
    
    * fix pattern
    
    * finish changing bucket section
    
    * fix python example
    
    * add removing section
    
    * refactor structure
    
    * fix CLI command
    
    * fix curl and cli examples
    
    * fix alias removing
    
    * redirect err output to std
    
    * fix cli example
    
    * document browsing bucket with web console
    
    * document web console and add images
    
    * add links
    
    * fix broken links
    
    * review
    
    * copy to 1.9.x docs
    
    * fix typo
    
    * fix path

---
+++ docs/guides/examples/curl/buckets_remove.sh
@@ -0,0 +1,6 @@
+export API_PATH="http://127.0.0.1:8383/api/v1"
+export AUTH_HEADER="Authorization: Bearer my-token"
+
+curl -X DELETE \
+  -H "${AUTH_HEADER}" \
+  -a "${API_PATH}"/b/bucket-to-remove
