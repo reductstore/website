dd97541b93c1d118513cef56f350429619c49a7d
Alexey Timin <atimin@gmail.com>
2024-04-21 10:35:38 +0200

    fix bigint issue

--- docs/guides/examples/js/src/data_ingestion_simple.mjs
+++ docs/guides/examples/js/src/data_ingestion_simple.mjs
@@ -5,6 +5,6 @@ const client = new Client("http://127.0.0.1:8383", { apiToken: "my-token"});
 const bucket = await client.getOrCreateBucket("bucket");
 
 // Send a record to the "entry-1" entry with the current timestamp
-const timestamp = Date.now() * 1000;
+const timestamp = BigInt(Date.now()) * 1000n;
 let record = await bucket.beginWrite("entry-1", timestamp);
 await record.write("Some binary data");
--- src/components/CodeSnippetExample/CodeExamples.ts
+++ src/components/CodeSnippetExample/CodeExamples.ts
@@ -24,7 +24,7 @@ const client = new Client("http://127.0.0.1:8383");
 const main = async () => {
   const bucket = await client.getOrCreateBucket("bucket");
 
-  const timestamp = Date.now() * 1000;
+  const timestamp = BigInt(Date.now()) * 1000n;
   let record = await bucket.beginWrite("entry-1", timestamp);
   await record.write("Hello, World!");
 
