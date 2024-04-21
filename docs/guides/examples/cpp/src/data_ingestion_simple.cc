163b2cd97fa26501ac67836ca253ef3bbffe5bfe
Alexey Timin <atimin@gmail.com>
2024-04-21 10:41:31 +0200

    fix api token in cpp

--- docs/guides/examples/cpp/src/data_ingestion_simple.cc
+++ docs/guides/examples/cpp/src/data_ingestion_simple.cc
@@ -1,13 +1,16 @@
 #include <reduct/client.h>
 #include <iostream>
+#include <cassert>
 
 using reduct::IBucket;
 using reduct::IClient;
+using reduct::Error;
 
 int main() {
     // Create a client instance, then get or create a bucket
-    auto client = IClient::Build("http://127.0.0.1:8383", {.api_token="my=token"});
+    auto client = IClient::Build("http://127.0.0.1:8383", {.api_token="my-token"});
     auto [bucket, create_err] = client->GetOrCreateBucket("my-bucket");
+    assert(create_err == Error::kOk);
 
     // Send a record to the "entry-1" entry with the current timestamp
     IBucket::Time ts = IBucket::Time::clock::now();
