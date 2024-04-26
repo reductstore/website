16a308fd8bb7a2f479018f243390d5f29aae32f5
Alexey Timin <atimin@gmail.com>
2024-04-26 10:58:51 +0200

    add curl

--- docs/guides/examples/curl/data_ingestion_annotating.sh
+++ docs/guides/examples/curl/data_ingestion_annotating.sh
@@ -1,4 +1,6 @@
 export API_PATH="http://127.0.0.1:8383/api/v1"
+export AUTH_HEADER="Authorization: Bearer my-token"
+
 # Send a record  with labels and content type
 export TIME=`date +%s000000`
 curl -d "Some binary data" \
