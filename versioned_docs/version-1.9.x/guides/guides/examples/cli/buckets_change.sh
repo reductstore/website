0e5020e05b114363f737bb5996a880a23193495d
Alexey Timin <atimin@gmail.com>
2024-04-29 23:01:50 +0200

    update links and fix 1.9.x

--- versioned_docs/version-1.9.x/guides/guides/examples/cli/buckets_change.sh
+++
@@ -1,3 +0,0 @@
-reduct-cli alias add local -L http://localhost:8383 -t "my-token"
-# Change the quota size of the bucket
-reduct-cli bucket update local/example-bucket --quota-size 10GB
--- versioned_docs/version-1.9.x/guides/guides/examples/cli/buckets_create.sh
+++
@@ -1,2 +0,0 @@
-reduct-cli alias add local -L http://localhost:8383 -t "my-token"
-reduct-cli bucket create local/my-bucket --quota-type FIFO --quota-size 1GB
