48c9aba55ee65f48cddb2d4a76860c4a52aacd56
Alexey Timin <atimin@gmail.com>
2024-05-06 21:30:32 +0200

    add snippets for cpp and rust

---
+++ docs/guides/examples/cpp/src/data_replication_create.cc
@@ -0,0 +1,31 @@
+#include <reduct/client.h>
+#include <iostream>
+#include <cassert>
+
+using reduct::IBucket;
+using reduct::IClient;
+using reduct::Error;
+
+int main() {
+    // Create a client with the server URL
+    auto client = IClient::Build("http://127.0.0.1:8383", {
+            .api_token = "my-token"
+    });
+
+    // Create a bucket with the name "my-bucket" and a FIFO quota of 1GB
+    IBucket::Settings settings;
+    settings.quota_type = IBucket::QuotaType::kFifo;
+    settings.quota_size = 1'000'000'000;
+    auto [bucket, create_err] = client->GetOrCreateBucket("my-bucket", settings);
+    assert(create_err == Error::kOk);
+
+    auto repl_err = client->CreateReplication("my-replication", IClient::ReplicationSettings{
+        .src_bucket = "my-bucket",
+        .dst_bucket = "demo",
+        .dst_host = "https://play.reduct.store",
+        .dst_token = "reductstore",
+        .entries = {"cpp-example"},
+        .include = {{"anomaly", "1"}},
+    });
+    assert(repl_err == Error::kOk);
+}
