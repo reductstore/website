0a67175bacf1a4c9d05feb2d651d6fa5ec2f7a9d
Alexey Timin <atimin@gmail.com>
2024-05-07 22:29:47 +0200

    draft ready

--- docs/guides/examples/cli/data_replication_browse.sh
+++ docs/guides/examples/cli/data_replication_browse.sh
@@ -1,7 +1,5 @@
 reduct-cli alias add local -L http://localhost:8383 -t "my-token"
-
 # List all replications
 reduct-cli replica ls local --full
-
 # Browse a specific replication
 reduct-cli replica show local/example-replication
--- docs/guides/examples/cli/data_replication_create.sh
+++ docs/guides/examples/cli/data_replication_create.sh
@@ -1,5 +1,4 @@
 reduct-cli alias add local -L http://localhost:8383 -t "my-token"
-
 # Create a source bucket
 reduct-cli bucket create local/src-bucket
 # Create a replication between the source bucket and the demo bucket at https://play.reduct.store
