d8d2ceaea9124b28a64bcc07925c0713c5f22211
Alexey Timin <atimin@gmail.com>
2024-05-08 18:55:59 +0200

    fix data_replication_manual.sh

--- docs/guides/examples/cli/data_replication_manual.sh
+++ docs/guides/examples/cli/data_replication_manual.sh
@@ -1,3 +1,3 @@
-reduct-cli alias add src-instance -L http://localhost:8383 -T my-token
-reduct-cli alias add dst-instance -L https://play.reduct.store -T reductstore
+reduct-cli alias add src-instance -L http://localhost:8383 -t my-token
+reduct-cli alias add dst-instance -L https://play.reduct.store -t reductstore
 reduct-cli cp src-instance/example-bucket dst-instance/demo --include "anomaly=true" --exclude "status=ok"
