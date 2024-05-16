bade1b67e79e6ea464854add3c290cb19d263f2f
Anthony <anthony.cavin.ac@gmail.com>
2024-05-16 10:50:48 +0200

    RS-270: Improve MQTT storage article (#47)
    * update blog with new date
    
    * new date
    
    * add links
    
    * polish article
    
    * remove unused chapter link
    
    * correct chapter link
    
    * remove external links and add tag
    
    * keep naviguation bar open
    
    * add summary table
    
    * correct summary table
    
    * move banner
    
    * update banner
    
    * update date
    
    * fix broken link

--- src/components/ConsultationBanner/index.tsx
+++ src/components/ConsultationBanner/index.tsx
@@ -11,9 +11,9 @@ const ConsultationBanner: React.FC = () => {
   return (
     <div className={styles.consultationBanner}>
       <h3 className={styles.title}>Book a Free Consultation</h3>
-      <p className={styles.subtitle}>Get expert advice for your project</p>
+      <p className={styles.subtitle}>Discuss your project requirements and get a free quote</p>
       <Link to={customFields.calendarLink as string} className={`button button--primary button--md ${styles.bookButton}`}>
-        Book Now
+        Book a 30-Minute Call with Me
       </Link>
     </div>
   );
