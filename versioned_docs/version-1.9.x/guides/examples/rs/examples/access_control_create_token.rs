e2154b901bb647ae8032948faf707a5e916c1885
Alexey Timin <atimin@gmail.com>
2024-05-24 12:12:42 +0200

    copy for stable version

---
+++ versioned_docs/version-1.9.x/guides/examples/rs/examples/access_control_create_token.rs
@@ -0,0 +1,23 @@
+use reduct_rs::{Permissions, QuotaType, ReductClient, ReductError};
+use tokio;
+
+#[tokio::main]
+async fn main() -> Result<(), ReductError> {
+    // Create a new client with the API URL and API token
+    let client = ReductClient::builder()
+        .url("http://127.0.0.1:8383")
+        .api_token("my-token")
+        .build();
+
+    // Create a token with read/write access to the bucket "example-bucket"
+    let permissions = Permissions{
+        full_access: false,
+        read: vec![String::from("example-bucket")],
+        write: vec![String::from("example-bucket")],
+    };
+    let token = client
+        .create_token("new-token", permissions).await;
+
+    println!("Generated token: {:?}", token);
+    Ok(())
+}
