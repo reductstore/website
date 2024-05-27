d3bcca5fffdcdaaa5770bc5dd20206bfb177173b
AnthonyCvn <anthony.cavin.ac@gmail.com>
2024-05-27 15:32:06 +0200

    improve white paper form

--- src/components/WhitePaperForm/index.tsx
+++ src/components/WhitePaperForm/index.tsx
@@ -25,11 +25,11 @@ export default function WhitePaperForm() {
   if (state.succeeded) {
     downloadWhitePaper();
     return (
-      <>
+      <div className="alert alert--success">
         <h2>Thank you for your interest!</h2>
         <p>Your white paper download should start shortly.</p>
         <p>If the download does not start, <strong><Link to={whitePaperUrl} target="_blank">click here</Link></strong> to download it manually.</p>
-      </>
+      </div>
     );
   }
 
@@ -40,33 +40,38 @@ export default function WhitePaperForm() {
 
       <form className={styles.whitePaperForm} onSubmit={handleSubmit}>
         <div className={styles.inputGroup}>
-          <label htmlFor="InputNameWhitePaper">Name</label>
+          <label htmlFor="InputNameWhitePaper">Your Name</label>
           <input
             type="text"
             id="InputNameWhitePaper"
             name="name"
+            placeholder="Your Name"
             className={styles.inputField}
+            required
           />
           <ValidationError prefix="Name" field="name" errors={state.errors} />
         </div>
 
         <div className={styles.inputGroup}>
-          <label htmlFor="InputCompanyWhitePaper">Company</label>
+          <label htmlFor="InputCompanyWhitePaper">Your Company</label>
           <input
             type="text"
             id="InputCompanyWhitePaper"
             name="company"
+            placeholder="Your Company"
+            required
             className={styles.inputField}
           />
           <ValidationError prefix="Company" field="company" errors={state.errors} />
         </div>
 
         <div className={styles.inputGroup}>
-          <label htmlFor="InputEmailWhitePaper">Email</label>
+          <label htmlFor="InputEmailWhitePaper">Your Email</label>
           <input
             type="email"
             id="InputEmailWhitePaper"
             name="email"
+            placeholder="Your Email"
             required
             className={styles.inputField}
           />
@@ -90,7 +95,7 @@ export default function WhitePaperForm() {
               target="_blank"
             >
               Privacy Policy
-            </Link>.
+            </Link>
           </div>
         </div>
       </form>
