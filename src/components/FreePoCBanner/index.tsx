fff79a4afe183e01725f7b376fc36d2605603b30
Anthony <anthony.cavin.ac@gmail.com>
2024-05-28 15:40:26 +0200

    RS-289: lead magnet #2 free PoC  integration (#52)
    * improve contact form
    
    * improve white paper form
    
    * add a countdown timer
    
    * free PoC form
    
    * free PoC Banner
    
    * add free PoC to ai-workflow use case
    
    * adapt to dark mode
    
    * countdown timer
    
    * remove unused hooks
    
    * adapt banner for mobile

---
+++ src/components/FreePoCBanner/index.tsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from 'react';
+import CountdownTimer from '../CountdownTimer';
+import styles from './styles.module.css';
+import Link from '@docusaurus/Link';
+
+interface FreePoCBannerProps {
+  targetDate: Date;
+  startDate: Date;
+  elementId: string;
+}
+
+const FreePoCBanner = ({ targetDate, startDate, elementId }: FreePoCBannerProps) => {
+  const [isFormPresent, setIsFormPresent] = useState(false);
+
+  useEffect(() => {
+    const formElement = document.getElementById(elementId);
+    setIsFormPresent(!!formElement);
+  }, [elementId]);
+
+  const handleClick = () => {
+    const formElement = document.getElementById(elementId);
+    if (formElement) {
+      const yOffset = -100;
+      const y = formElement.getBoundingClientRect().top + window.scrollY + yOffset;
+      window.scrollTo({ top: y, behavior: 'smooth' });
+    }
+  };
+
+  if (!isFormPresent) {
+    return null;
+  }
+
+  return (
+    <div className={styles.banner}>
+      <span className={styles.bannerText}>
+        <Link onClick={handleClick}>Get a Free PoC Integration for your Project</Link>
+        <CountdownTimer targetDate={targetDate} startDate={startDate} size={50} />
+      </span>
+    </div>
+  );
+};
+
+export default FreePoCBanner;
