e4db7b3d2e89cb4117cfdda42bcc16497f37cb8d
AnthonyCvn <anthony.cavin.ac@gmail.com>
2024-06-14 16:27:36 +0200

    update use-case page

--- src/pages/use-cases/index.tsx
+++ src/pages/use-cases/index.tsx
@@ -1,35 +1,20 @@
 import React from 'react';
 import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
 import SimpleHeader from "@site/src/components/SimpleHeader";
+import styles from "./styles.module.css";
+import UseCaseCards from '@site/src/components/UseCaseCards';
 import clsx from 'clsx';
-import styles from './styles.module.css';
 
 export default function UseCases(): JSX.Element {
   return (
     <Layout
-      title="Explore Use Case Scenarios"
+      title="Explore Typical Use Cases"
       description="Learn how our solutions can help you transform your infrastructure."
     >
       <main>
-        <SimpleHeader pageTitle="Explore Use Case Scenarios for ReductStore" />
-
+        <SimpleHeader pageTitle="Explore Typical Use Cases" />
         <div className={clsx("container", styles.useCasesContainer)}>
-          {useCases.map((useCase, index) => (
-            <div key={index} className={clsx("card", styles.useCaseCard)}>
-              <div className="card__header">
-                <h3>{useCase.title}</h3>
-              </div>
-              <div className="card__body">
-                <p>{useCase.description}</p>
-              </div>
-              <div className="card__footer">
-                <Link to={useCase.link} className="button button--primary button--block">
-                  Learn More &rarr;
-                </Link>
-              </div>
-            </div>
-          ))}
+          <UseCaseCards useCases={useCases} />
         </div>
       </main>
     </Layout >
@@ -39,12 +24,42 @@ export default function UseCases(): JSX.Element {
 const useCases = [
   {
     title: "High Frequency Vibration Data Storage",
-    description: "Learn how ReductStore stands out with vibration sensor data storage and management.",
+    description: "Learn how to store and manage high-frequency vibration data.",
     link: "/use-cases/vibration-sensors"
   },
   {
-    title: "Optimize Your AI Workflows",
-    description: "Learn how ReductStore can improve the performance of your projects with an optimal infrastructure.",
+    title: "AI Infrastructure",
+    description: "Learn how to store and manage data for AI workflows.",
     link: "/use-cases/ai-workflows"
+  },
+  {
+    title: "Computer Vision Applications",
+    description: "Learn how to store data for computer vision applications.",
+    link: "/blog/tutorials/computer-vision/3-ways-stora-data-for-computer-vision-applications"
+  },
+  {
+    title: "MQTT Data Storage",
+    description: "Learn how to store and manage MQTT data.",
+    link: "/blog/advice/database/mqtt-data-storage"
+  },
+  {
+    title: "Robot Operating System (ROS)",
+    description: "Learn how to store and manage image data for ROS-based computer vision applications.",
+    link: "/blog/tutorials/ros/optimal-image-storage-solutions-for-ros-based-computer-vision"
+  },
+  {
+    title: "Kafka Data Sink",
+    description: "Learn how to implement a Kafka data sink.",
+    link: "/blog/tutorial/datastreaming/kafka/data-sink-guide"
+  },
+  {
+    title: "Anomaly Detection",
+    description: "Learn how to implement an open-source AI anomaly detection system.",
+    link: "/blog/computer-vision/edge-computing/ai/Implementing-open-source-ai-anomaly-detection"
+  },
+  {
+    title: "Pytorch Data Streaming",
+    description: "Learn how to implement database data streaming with Pytorch.",
+    link: "/blog/ai/datastreaming/pytorch/implement-database-data-streaming-pytorch"
   }
 ];
