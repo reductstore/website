b91b2ebfb073d1bcdb775da259279cff26a0ab4d
AnthonyCvn <anthony.cavin.ac@gmail.com>
2024-07-31 14:15:10 +0200

    remove promotion

--- src/components/PromotionalBanner/index.tsx
+++ src/components/PromotionalBanner/index.tsx
@@ -1,30 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './styles.module.css';
 import { FaCheckCircle } from 'react-icons/fa';
+import FreePoCForm from '../FreePoCForm';
 
 const PromotionalBanner = () => {
   const checkedIcon = <FaCheckCircle color="green" size="1em" />;
+  const [openModal, setOpenModal] = useState(false);
+  const [clostModal, setCloseModal] = useState(false);
 
   return (
-    <div className="alert alert--warning margin-bottom--lg">
-      <div className="container">
-        <div >
-          <h3>Limited Offer for Early Adopters: Get 80% Off + Free PoC!</h3>
-          <p>
-            We are opening up just a few more seats for the <b>next 5 early adopters</b> who need our solution with the code <code>EARLYADOPTER80</code>.
-          </p>
-          <p>
-            Secure your spot today, and when you're ready to start, you'll get:
-          </p>
-          <ul className={styles.benefitsList}>
-            <li>{checkedIcon} Free Proof of Concept (PoC)</li>
-            <li>{checkedIcon} Priority customer support</li>
-            <li>{checkedIcon} Assistance with data migration</li>
-            <li>{checkedIcon} 30-day money-back guarantee</li>
-          </ul>
+    <>
+      <div className="alert alert--warning margin-bottom--lg">
+        <div className="container">
+          <div>
+            <h3>Free Evaluation and Proof of Concept (PoC)</h3>
+            <p>
+              Secure your spot today, and our team will reach out to you to understand your requirements and provide a custom evaluation tailored to your project.
+            </p>
+            <ul className={styles.benefitsList}>
+              <li>{checkedIcon} Consultation call to understand your requirements</li>
+              <li>{checkedIcon} Custom evaluation tailored to your project</li>
+              <li>{checkedIcon} Active support during the PoC phase and beyond</li>
+            </ul>
+            <div className={styles.buttonContainer}>
+              <button className="button button--primary" >
+                Request Free Evaluation and Proof of Concept (PoC)
+              </button>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+      <div className={styles.form}>
+        <FreePoCForm elementId="free-poc-form" />
+      </div>
+    </>
   );
 };
 
