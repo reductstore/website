457a962bf035c14e65545fb9cb3412441166f725
Anthony <anthony.cavin.ac@gmail.com>
2024-07-31 14:13:14 +0200

    RS-369: Update whitepaper page (#78)
    * new white paper
    
    * update white paper name
    
    * install new font awesome icons
    
    * bullet point component
    
    * modal component
    
    * fix white background in dark mode
    
    * new white paper page description
    
    * update white paper

---
+++ src/components/BulletPointItem/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle, IconDefinition } from '@fortawesome/free-solid-svg-icons';
+import styles from './styles.module.css';
+
+type BulletPointItemProps = {
+  children: React.ReactNode;
+  icon?: IconDefinition;
+  size?: "xs" | "sm" | "lg" | "1x" | "2x" | "3x" | "4x" | "5x";
+};
+
+export default function BulletPointItem({ children, icon, size = "1x" }: BulletPointItemProps) {
+  return (
+    <li className={styles.bulletPointItem}>
+      <FontAwesomeIcon
+        icon={icon || faCheckCircle}
+        className={styles.bulletIcon}
+        size={size}
+      />
+      {children}
+    </li>
+  );
+}
