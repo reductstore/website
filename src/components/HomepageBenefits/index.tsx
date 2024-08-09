0689a2279c982b9698ff233a1afbd902e4319f90
Anthony <anthony.cavin.ac@gmail.com>
2024-08-09 15:15:52 +0200

    RS-381: Use illustrations for features on the landing page (#81)

--- src/components/HomepageBenefits/index.tsx
+++ src/components/HomepageBenefits/index.tsx
@@ -1,21 +1,28 @@
 import React from "react";
-import {
-  FaProjectDiagram,
-  FaHdd,
-  FaChartLine,
-} from "react-icons/fa";
+import useBaseUrl from '@docusaurus/useBaseUrl';
+import ThemedImage from '@theme/ThemedImage';
+
 import styles from "./styles.module.css";
 
-function Benefits({ IconComponent, title, description }) {
+interface BenefitsValue {
+  path_light: string;
+  path_dark: string;
+  title: string;
+  description: string;
+}
+
+function Benefits({ path_light, path_dark, title, description }: BenefitsValue) {
   return (
     <div className="col col--4">
       <div className={styles.benefit}>
-        <div className={styles.benefitIcon}>
-          <IconComponent
-            size="5em"
-            style={{ color: "var(--ifm-color-primary)" }}
-          />
-        </div>
+        <ThemedImage
+          className={styles.benefitIcon}
+          alt={title}
+          sources={{
+            light: useBaseUrl(path_light),
+            dark: useBaseUrl(path_dark),
+          }}
+        />
         <h3>{title}</h3>
         <p>{description}</p>
       </div>
@@ -28,17 +35,20 @@ export default function HomepageBenefits() {
     <section className={styles.benefits}>
       <div className="row">
         <Benefits
-          IconComponent={FaProjectDiagram}
+          path_light="/img/benefits/simplify_your_infrastructure.webp"
+          path_dark="/img/benefits/simplify_your_infrastructure_black.webp"
           title="Simplify Your Infrastructure"
           description="Merge blob and time series functionalities, reducing the need for multiple databases."
         />
         <Benefits
-          IconComponent={FaHdd}
+          path_light="/img/benefits/stay_in_control_of_your_data.webp"
+          path_dark="/img/benefits/stay_in_control_of_your_data_black.webp"
           title="Stay In Control Of Your Data"
           description="Customize real-time data retention policies and replication strategies."
         />
         <Benefits
-          IconComponent={FaChartLine}
+          path_light="/img/benefits/handle_large_data_volume.webp"
+          path_dark="/img/benefits/handle_large_data_volume_black.webp"
           title="Handle Large Data Volumes"
           description="Store billions of time-stamped blobs with AI labels and access them with low latency."
         />
