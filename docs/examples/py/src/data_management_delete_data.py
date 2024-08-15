88ff427860cc31cd1bc86bfa6b28f68c2d7e8f05
Alexey Timin <atimin@gmail.com>
2024-08-15 12:42:56 +0200

    finish examples

--- docs/examples/py/src/data_management_delete_data.py
+++ docs/examples/py/src/data_management_delete_data.py
@@ -4,7 +4,7 @@ from reduct import Client, Bucket
 async def browse_buckets():
     # Create a client with the base URL and API token
     async with Client("http://localhost:8383", api_token="my-token") as client:
-        # Browse all buckets and print their information
+        # Get bucket to remove
         bucket: Bucket = await client.get_bucket("bucket-to-remove")
 
         # Delete only entry with name "example-entry"
