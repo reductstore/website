6aa9570e2ee9059b6757a5d36f58fcc2680dc43b
Alexey Timin <atimin@gmail.com>
2024-08-20 17:29:44 +0200

    RS-378: Release  ReductStore 1.11 (#87)
    * release documentation
    
    * write release note
    
    * publish tomorrow
    
    * little improvements
    
    * review

---
+++ versioned_docs/version-1.11.x/examples/js/src/access_control_browse_tokens.mjs
@@ -0,0 +1,16 @@
+import {Client} from "reduct-js";
+
+// Create a new client with the server URL and an API token
+const client = new Client("http://127.0.0.1:8383", {apiToken: "my-token"});
+
+// Browse all tokens and print their information
+const tokenList = await client.getTokenList();
+for (const token of tokenList) {
+    console.log(`Token: ${token.name}`);
+    console.log(`Created: ${token.createdAt}`);
+    console.log(`Provisioned: ${token.isProvisioned}`);
+
+    // Get detailed information about a specific token
+    const tokenInfo = await client.getToken(token.name);
+    console.log(`Permissions: ${tokenInfo.permissions}`);
+}
---
+++ versioned_docs/version-1.11.x/examples/js/src/data_querying_filter.mjs
@@ -0,0 +1,16 @@
+import {Client} from "reduct-js";
+
+// Create a client instance, then get or create a bucket
+const client = new Client("http://127.0.0.1:8383", {apiToken: "my-token"});
+const bucket = await client.getBucket("example-bucket")
+
+// Query 10 photos from "imdb" entry which taken in 2006 but don't contain "Rowan Atkinson"
+for await (const record of bucket.query("imdb", undefined, undefined, {
+    limit: 10, include: {photo_taken: "2006"},
+    exclude: {name: "b'Rowan Atkinson'"}
+})) {
+    console.log("Name", record.labels.name);
+    console.log("Phot taken", record.labels.photo_taken);
+    console.log("Gender", record.labels.gender);
+    await record.readAsString();
+}
