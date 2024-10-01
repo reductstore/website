a535f9b807f31ade5211408376be161f0aa87558
AnthonyCvn <anthony.cavin.ac@gmail.com>
2024-10-01 21:20:49 +0200

    new banner and header with waiting list form

---
+++ src/components/SimpleHeaderWithForm/index.tsx
@@ -0,0 +1,46 @@
+import React, { SVGProps } from "react";
+import styles from "./styles.module.css";
+import Heading from "@theme/Heading";
+import WaitingListForm from "../WaitingListForm";
+import { FaCheck } from "react-icons/fa"; // Importing icon
+
+interface SimpleHeaderWithFormProps {
+  title: string;
+  subtitle: string;
+  benefits: string[];
+  imageSvg: React.ReactNode;
+  headerTag?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+}
+
+function SimpleHeaderWithForm({
+  title,
+  subtitle,
+  benefits,
+  imageSvg,
+  headerTag = "h1",
+}: SimpleHeaderWithFormProps) {
+  return (
+    <header className={styles.Banner}>
+      <div className={`container ${styles.BannerContent}`}>
+        <div className={styles.BannerLeft}>
+          <Heading as={headerTag} className={styles.BannerTitle}>
+            {title}
+          </Heading>
+          <p className={styles.BannerSubtitle}>{subtitle}</p>
+          <WaitingListForm />
+          <ul className={styles.BenefitsList}>
+            {benefits.map((benefit, index) => (
+              <li key={index} className={styles.BenefitItem}>
+                <FaCheck className={styles.BenefitIcon} />
+                {benefit}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className={styles.BannerRight}>{imageSvg}</div>
+      </div>
+    </header>
+  );
+}
+
+export default SimpleHeaderWithForm;
