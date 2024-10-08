283af5d11bb1a338fb86d5cfc061a47a3bd3e2d3
Alexey Timin <atimin@gmail.com>
2024-10-08 10:24:57 +0200

    RS-464:  Release v1.12 (#103)
    * add version 1.12
    
    * write release note
    
    * format
    
    * add more versions to test
    
    * fix docker tags
    
    * add rename bucket and entry API to v1.12
    
    * update benchmarks
    
    * formatter
    
    * review

---
+++ versioned_docs/version-1.12.x/examples/rs/examples/data_management_delete_query.rs
@@ -0,0 +1,55 @@
+use std::time::{Duration, SystemTime};
+
+use bytes::Bytes;
+use reduct_rs::{ErrorCode, RecordBuilder, ReductClient, ReductError};
+use tokio;
+
+#[tokio::main]
+async fn main() -> Result<(), ReductError> {
+    // Create a client instance, then get or create a bucket
+    let client = ReductClient::builder()
+        .url("http://127.0.0.1:8383")
+        .api_token("my-token")
+        .build();
+    let bucket = client.create_bucket("test").exist_ok(true).send().await?;
+
+    // Send some records to the "rs-example" entry
+    let timestamp = SystemTime::now();
+    bucket
+        .write_record("rs-example")
+        .timestamp(timestamp)
+        .data(Bytes::from("Some binary data"))
+        .send()
+        .await?;
+
+    bucket
+        .write_record("rs-example")
+        .timestamp(timestamp  + Duration::from_secs(1))
+        .data(Bytes::from("Some more binary data"))
+        .send()
+        .await?;
+
+    //  Delete all records within a time range
+    let removed_records = bucket
+        .remove_query("rs-example")
+        .start(timestamp)
+        .stop(timestamp + Duration::from_secs(2))
+        .send()
+        .await?;
+    assert_eq!(removed_records, 2);
+
+    // You can also delete all records with a specific label
+    bucket
+        .remove_query("rs-example")
+        .add_include("label1", "value1")
+        .send()
+        .await?;
+
+    // Or each N-th record
+    bucket
+        .remove_query("rs-example")
+        .each_n(2)
+        .send()
+        .await?;
+    Ok(())
+}
