4b729eb459c2677d8eae51cc863e9c4b3e927e8c
AnthonyCvn <anthony.cavin.ac@gmail.com>
2024-10-15 21:55:09 +0200

    adapt sliding banner logic

--- src/components/SlidingBanner/index.tsx
+++ src/components/SlidingBanner/index.tsx
@@ -2,35 +2,28 @@ import React, { useEffect, useState } from "react";
 import Link from "@docusaurus/Link";
 import Cookies from "js-cookie";
 import styles from "./styles.module.css";
-import useCookieConsentStore from "@site/src/store/useCookieConsentStore";
+import useBannertStore from "@site/src/store/useBannertStore";
 import { useLocation } from "@docusaurus/router";
 const whitePaperImage =
   require("@site/static/img/whitepaper/whitepaper.webp").default;
 
 const SlidingBanner: React.FC = () => {
   const [visible, setVisible] = useState(false);
-  const { isModalOpen, wasBannerShown, setBannerShown } =
-    useCookieConsentStore();
+  const { wasBannerShown, setBannerShown } = useBannertStore();
   const pagePath = useLocation().pathname;
   const isCorrectPath = !pagePath.endsWith("/blog");
 
   useEffect(() => {
     const isMobile = window.innerWidth <= 768;
     const isDismissed = Cookies.get("reductstore_bannerDismissed") === "true";
-    if (
-      isCorrectPath &&
-      !isModalOpen &&
-      !wasBannerShown &&
-      !isMobile &&
-      !isDismissed
-    ) {
+    if (isCorrectPath && !wasBannerShown && !isMobile && !isDismissed) {
       const timeout = setTimeout(() => {
         setVisible(true);
         setBannerShown(true);
       }, 13_000);
       return () => clearTimeout(timeout);
     }
-  }, [isModalOpen, wasBannerShown, setBannerShown, setVisible, isCorrectPath]);
+  }, [wasBannerShown, setBannerShown, setVisible, isCorrectPath]);
 
   const handleClose = () => {
     setVisible(false);
