6033453055525284e63db7bcc31066484b14c9a9
AnthonyCvn <anthony.cavin.ac@gmail.com>
2024-10-15 21:54:33 +0200

    remove vanilla cookie consent banner

--- src/theme/Root.js
+++ src/theme/Root.js
@@ -1,22 +1,5 @@
-import React, { useState } from "react";
-import { useEffect } from "react";
-import { initializeCookieConsent } from "./cookieConsent";
-import { useLocation } from "@docusaurus/router";
-import useCookieConsentStore from "@site/src/store/useCookieConsentStore";
+import React from "react";
 
 export default function Root({ children }) {
-  const location = useLocation();
-  const [initialized, setInitialized] = useState(false);
-  const { setModalOpen } = useCookieConsentStore.getState();
-  useEffect(() => {
-    if (initialized) {
-      setModalOpen(false);
-      return;
-    }
-    setTimeout(() => {
-      initializeCookieConsent();
-      setInitialized(true);
-    }, 1000);
-  }, [location.pathname]);
   return <>{children}</>;
 }
