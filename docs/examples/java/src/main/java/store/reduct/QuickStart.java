2f3c80e515666b98a9bbd876c96e85b1749e95e8
Alexey Timin <atimin@gmail.com>
2024-10-29 13:25:14 +0100

    update reduct-java

--- docs/examples/java/src/main/java/store/reduct/QuickStart.java
+++ docs/examples/java/src/main/java/store/reduct/QuickStart.java
@@ -13,39 +13,42 @@ import java.util.Iterator;
 public class QuickStart {
     public static void main(String[] args) {
          // 1. Create a ReductStore client
-        ServerProperties properties = new ServerProperties(false, "127.0.0.1", 8383);
+        ServerProperties properties = ServerProperties
+                .builder()
+                .url("http://localhost:8383")
+                .apiToken("my-token")
+                .build();
+
         HttpClient httpClient = HttpClient.newHttpClient();
-        ReductClient client = new ReductClient(properties, httpClient, "my-token");
+        ReductClient client = new ReductClient(properties, httpClient);
 
         // 2. Get or create a bucket with 1Gb quota
         BucketSettings settings = BucketSettings.builder()
                 .quotaType(QuotaType.FIFO)
                 .quotaSize(1_000_000_000)
-                //.exits(true) TODO: add this method to BucketSettings
+                .exists(true)
                 .build();
         Bucket bucket = client.createBucket("my-bucket1", settings);
 
         // 3. Write some data with timestamps in the 'sensor-1' entry
         long timestamp = System.currentTimeMillis() * 1000; // in microseconds
         Record record = Record.builder()
-                .entryName("sensor-1")
                 .timestamp(timestamp)
                 .type("application/octet-stream")
                 .body("Record #1".getBytes())
                 .build();
 
-        bucket.writeRecord(record);
+        bucket.writeRecord("sensor-1", record);
         record = Record.builder()
-                .entryName("sensor-2")
                 .timestamp(timestamp + 1000_000)
                 .type("application/json")
                 .length(9)
                 .body("Record #2".getBytes())
                 .build();
-        bucket.writeRecord(record);
+        bucket.writeRecord("sensor-1", record);
 
         // 4. Read the data back
-        for (Iterator<Record> it = bucket.query("sensor-1", (int) timestamp, (int) timestamp + 1000_000, 60); it.hasNext(); ) {
+        for (Iterator<Record> it = bucket.query("sensor-1", timestamp, timestamp + 2000_000, 60L); it.hasNext(); ) {
             Record rec = it.next();
 
             System.out.println("Record timestamp: " + rec.getTimestamp());
