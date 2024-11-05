f71470335c30fd4f1aa50d2ed16c845dc245420b
AnthonyCvn <anthony.cavin.ac@gmail.com>
2024-11-05 23:33:35 +0100

    add blog subscription form

--- src/theme/BlogPostItem/index.js
+++ src/theme/BlogPostItem/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import BlogPostItem from "@theme-original/BlogPostItem";
 import SocialShareBar from "@site/src/components/SocialShareBar";
 import SlidingBanner from "@site/src/components/SlidingBanner";
+import BlogForm from "@site/src/components/BlogForm";
 
 export default function BlogPostItemWrapper(props) {
   const { frontMatter } = props.children.type;
@@ -9,6 +10,7 @@ export default function BlogPostItemWrapper(props) {
     <>
       <BlogPostItem {...props} />
       <SocialShareBar frontMatter={frontMatter} />
+      <BlogForm frontMatter={frontMatter} />
       <SlidingBanner />
     </>
   );
