ec80d3e81a36741417fc1596bccbfcf8badd0f3e
Alexey Timin <atimin@gmail.com>
2024-11-06 13:57:25 +0100

    fix curl examples

--- docs/examples/curl/data_querying_filter.sh
+++ docs/examples/curl/data_querying_filter.sh
@@ -6,7 +6,7 @@ AUTH_HEADER="Authorization: Bearer my-token"
 
 # Query 10 photos from "imdb" entry which taken in 2006 but don't contain "Rowan Atkinson"
 ID=`curl -H "${AUTH_HEADER}" \
-  -d '{"query_type": "QUERY", "include": {"photo_taken": 2006}, "exclude": {"name": "b%39Rowan%20Atkinson%39"}, "limit": 10}' \
+  -d '{"query_type": "QUERY", "include": {"photo_taken": "2006"}, "exclude": {"name": "b%39Rowan%20Atkinson%39"}, "limit": 10}' \
   -X POST -a "${API_PATH}/b/example-bucket/imdb/q" | jq -r ".id"`
 
 # Fetch the data (without batching) until the end
