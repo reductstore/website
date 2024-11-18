6eda2f38cb0626f7e1986a480d3f8fb0cd84e595
Alexey Timin <atimin@gmail.com>
2024-11-18 21:10:37 +0100

    add formatter to pre-commit

--- src/components/docs/InstallClientCLI/CargoInstall.tsx
+++ src/components/docs/InstallClientCLI/CargoInstall.tsx
@@ -10,9 +10,13 @@ cargo install reduct-cli
     <>
       <CodeBlock className="language-bash">{binaryInstall}</CodeBlock>
       <p>
-        You may need to install the latest version of Rust. Read the <b>
-          <a href="https://www.rust-lang.org/tools/install">official Rust installation guide</a>
-        </b> for more information.
+        You may need to install the latest version of Rust. Read the{" "}
+        <b>
+          <a href="https://www.rust-lang.org/tools/install">
+            official Rust installation guide
+          </a>
+        </b>{" "}
+        for more information.
       </p>
     </>
   );
