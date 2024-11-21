a3fc351955c5abe4dd61dcd84ef4e7f52671b3bd
AnthonyCvn <anthony.cavin.ac@gmail.com>
2024-11-21 12:10:04 +0100

    add rust example to rename entries

--- docs/examples/rs/examples/data_management_rename_entries.rs
+++ docs/examples/rs/examples/data_management_rename_entries.rs
@@ -1,3 +1,25 @@
-reduct-cli alias add local -L http://localhost:8383 -t "my-token"
-# Delete records with the specified timestamps
-reduct-cli rm local/example-bucket --entries cats --time 10 20
+use reduct_rs::{ReductClient, ReductError};
+use tokio;
+
+#[tokio::main]
+async fn main() -> Result<(), ReductError> {
+    // Create a new client with the API URL and API token
+    let client = ReductClient::builder()
+        .url("http://127.0.0.1:8383")
+        .api_token("my-token")
+        .build();
+
+    // Get the bucket 'example-bucket'
+    let bucket = client.get_bucket("example-bucket").await?;
+
+    // Rename the entry 'entry_1' to 'entry_2'
+    bucket.rename_entry("entry_1", "entry_2").await?;
+
+    // Check that the entry was renamed
+    let entries = bucket.entries().await?;
+    let entry_names: Vec<String> = entries.iter().map(|entry| entry.name.clone()).collect();
+    assert!(entry_names.contains(&"entry_2".to_string()));
+    assert!(!entry_names.contains(&"entry_1".to_string()));
+
+    Ok(())
+}
