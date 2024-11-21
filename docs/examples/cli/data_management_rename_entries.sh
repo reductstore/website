6cf58368d30812bce04ac005fdc51a4879ce48f0
AnthonyCvn <anthony.cavin.ac@gmail.com>
2024-11-21 13:40:56 +0100

    adapt CLI test for clarity

--- docs/examples/cli/data_management_rename_entries.sh
+++ docs/examples/cli/data_management_rename_entries.sh
@@ -1,2 +1,7 @@
+#!/bin/bash
+set -e -x
+
 reduct-cli alias add local -L http://localhost:8383 -t "my-token"
+
+# Rename the entry entry_1 to entry_2 in the bucket example-bucket
 reduct-cli bucket rename --only-entry entry_1 local/example-bucket entry_2
