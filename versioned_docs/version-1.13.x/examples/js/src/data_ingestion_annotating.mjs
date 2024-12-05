c01cbf07405f3f17f606b7389a4d6f28a517ef14
Alexey Timin <atimin@gmail.com>
2024-12-05 13:52:42 +0100

    RS-542: Release v1.13 (#132)
    * release v1.13.0
    
    * fix build
    
    * fix example path
    
    * release note
    
    * capitlize title

---
+++ versioned_docs/version-1.13.x/examples/js/src/data_ingestion_annotating.mjs
@@ -0,0 +1,17 @@
+import { Client } from "reduct-js";
+
+// Create a client instance, then get or create a bucket
+const client = new Client("http://127.0.0.1:8383", { apiToken: "my-token" });
+const bucket = await client.getOrCreateBucket("bucket");
+
+// Send a record  with labels and content type
+const timestamp = BigInt(Date.now()) * 1000n;
+let record = await bucket.beginWrite("entry-1", {
+  timestamp: timestamp,
+  contentType: "plain/text",
+  labels: {
+    name: "example",
+    type: "simple",
+  },
+});
+await record.write("Some binary data");
