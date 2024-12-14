800ea8ea6878ac459b4d72066948f94b999ddfc0
Alexey Timin <atimin@gmail.com>
2024-12-14 21:47:44 +0100

    fix folder

--- versioned_docs/version-1.13.x/examples/cpp/src/data_querying_filter.sh
+++
@@ -1,4 +0,0 @@
-reduct-cli alias add local -L http://localhost:8383 -t "my-token"
-
-# Query 10 photos from "imdb" entry which taken after 2006 with the face score less than 4
-reduct-cli cp local/example-bucket ./export --when='{"&photo_taken": {"$gt": 2006}, "&face_score": {"$lt": 4}}' --limit 10 --with-meta
