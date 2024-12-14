9c6b3cdc197429503943bffd48e1d328c9cd735b
Alexey Timin <atimin@gmail.com>
2024-12-14 22:06:37 +0100

    RS-552: Add when condition in CLI examples (#134)
    * add when condition in cli examples
    
    * update stable version
    
    * fix folder

--- versioned_docs/version-1.13.x/examples/cli/data_management_delete_query.sh
+++ versioned_docs/version-1.13.x/examples/cli/data_management_delete_query.sh
@@ -6,7 +6,7 @@ reduct-cli alias add local -L http://localhost:8383 -t "my-token"
 reduct-cli rm local/example-bucket --entries entry_1 --start 10 --stop 20
 
 # You can also delete all records with a specific label
-reduct-cli rm local/example-bucket --entries entry_1 --include label1=value1
+reduct-cli rm local/example-bucket --entries entry_1 --when '{"&key1": {"$eq": "value1"}}'
 
 # Or each N-th record
 reduct-cli rm local/example-bucket --entries entry_1 --each-n 2
--- versioned_docs/version-1.13.x/examples/cli/data_querying_filter.sh
+++ versioned_docs/version-1.13.x/examples/cli/data_querying_filter.sh
@@ -1,4 +1,4 @@
 reduct-cli alias add local -L http://localhost:8383 -t "my-token"
 
-# Query 10 photos from "imdb" entry which taken in 2006 but don't contain "Rowan Atkinson"
-reduct-cli cp local/example-bucket ./export --include "photo_taken=2011" --exclude "name=b'Rowan Atkinson'" --limit 100 --with-meta
+# Query 10 photos from "imdb" entry which taken after 2006 with the face score less than 4
+reduct-cli cp local/example-bucket ./export --when='{"&photo_taken": {"$gt": 2006}, "&face_score": {"$lt": 4}}' --limit 10 --with-meta
