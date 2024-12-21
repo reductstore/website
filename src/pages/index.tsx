1c4fe5a94c4e3e4f29061f30dbf474e347f13fe2
AnthonyCvn <anthony.cavin.ac@gmail.com>
2024-12-21 17:12:07 +0100

    update benefits

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -65,7 +65,7 @@ export default function Home(): JSX.Element {
       <main>
         <div className="container">
           <HomepageBenefits />
-          <PerformanceComparison />
+
           <hr />
           <HomepageStats />
           <hr />
