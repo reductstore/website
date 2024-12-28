2d387a6b42d8864a35b73221151745e32880cf22
Alexey Timin <atimin@gmail.com>
2024-12-28 16:56:12 +0100

    update rust documentation

--- docs/examples/cli/data_replication_manual.sh
+++ docs/examples/cli/data_replication_manual.sh
@@ -1,3 +1,6 @@
 reduct-cli alias add src-instance -L http://localhost:8383 -t my-token
 reduct-cli alias add dst-instance -L https://play.reduct.store -t reductstore
-reduct-cli cp src-instance/example-bucket dst-instance/demo --include "anomaly=true" --exclude "status=ok"
+reduct-cli cp src-instance/example-bucket dst-instance/demo --where '{
+  "&status": {"$ne": "ok"},
+  "&anomaly": {"$eq": true}
+}'
