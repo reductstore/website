99a014f864ded5cbedd503a6308fa04ca7fd521b
Anthony <anthony.cavin.ac@gmail.com>
2025-01-14 14:46:01 +0100

    KAN-93: Data Lakehouse for Manufacturing (#140)
    * blog article text
    
    * add performance comparison table
    
    * add images
    
    * new links and images
    
    * prettier fmt
    
    * new intro image
    
    * update to introduce ReductStore as a component of a data lakehouse
    
    * update introduction picture
    
    * update social image
    
    * fix broken link
    
    * link to cloud solution

---
+++ src/components/tables/PerformanceComparison/index.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import clsx from "clsx";
+import styles from "./styles.module.css";
+import { PerformanceComparisonData } from "./PerformanceComparisonData";
+import Link from "@docusaurus/Link";
+import PerformanceComparisonTable from "./PerformanceComparisonTable";
+
+export default function PerformanceComparison(): JSX.Element {
+  const [activeTab, setActiveTab] = useState("TimescaleDB");
+  const tabNameMap = {
+    TimescaleDB: "TimescaleDB",
+    MongoDB: "MongoDB",
+    MinIO: "MinIO",
+  };
+  return (
+    <>
+      <ul className={clsx("tabs", styles.tabs)}>
+        {["TimescaleDB", "MongoDB", "MinIO"].map((tab) => (
+          <li
+            key={tab}
+            className={clsx(
+              "tabs__item",
+              activeTab === tab && "tabs__item--active",
+            )}
+            onClick={() => setActiveTab(tab)}
+          >
+            vs {tabNameMap[tab]}
+          </li>
+        ))}
+      </ul>
+      <div className={styles.tabContent}>
+        <>
+          {activeTab && PerformanceComparisonData[activeTab] && (
+            <PerformanceComparisonTable
+              data={PerformanceComparisonData[activeTab]}
+            />
+          )}
+          <p>
+            <Link to={PerformanceComparisonData[activeTab].url}>
+              See the full {PerformanceComparisonData[activeTab].title}{" "}
+              benchmark.
+            </Link>
+          </p>
+        </>
+      </div>
+    </>
+  );
+}
