e7ecddd18a2582c216ad6bacff5bc8d7f2fb8bb1
Alexey Timin <atimin@reduct.store>
2025-02-26 20:29:13 +0100

    RS-604:  Release v1.14 (#160)
    * release v1.14
    
    * little improvements
    
    * update reduct-js
    
    * polish text and add graphics
    
    * update ci
    
    * fix links
    
    * Update blog/2025-02-25-reductstore-v1_14_0-released/index.mdx
    
    Co-authored-by: Anthony <anthony.cavin.ac@gmail.com>
    
    * Update blog/2025-02-25-reductstore-v1_14_0-released/index.mdx
    
    Co-authored-by: Anthony <anthony.cavin.ac@gmail.com>
    
    ---------
    
    Co-authored-by: Anthony <anthony.cavin.ac@gmail.com>

---
+++ versioned_docs/version-1.14.x/examples/cpp/src/data_querying_timestamp.cc
@@ -0,0 +1,49 @@
+#include <reduct/client.h>
+#include <iostream>
+#include <cassert>
+
+using reduct::IBucket;
+using reduct::IClient;
+using reduct::Error;
+
+using std::chrono_literals::operator ""s;
+
+int main() {
+    // Create a client instance, then get or create a bucket
+    auto client = IClient::Build("http://127.0.0.1:8383", {.api_token="my-token"});
+    auto [bucket, create_err] = client->GetOrCreateBucket("my-bucket");
+    assert(create_err == Error::kOk);
+
+    // Send a record  with labels and content type
+    IBucket::Time ts = IBucket::Time::clock::now();
+    auto err = bucket->Write("cpp-example", ts,[](auto rec) {
+        rec->WriteAll("Some binary data");
+    });
+    assert(err == Error::kOk);
+
+    // Query records in a time range
+    err = bucket->Read("cpp-example", ts, [](auto rec) {
+        // Print metadata
+        std::cout << "Timestamp: " <<  rec.timestamp.time_since_epoch().count() << std::endl;
+        std::cout << "Content Length: " << rec.size << std::endl;
+        std::cout << "Content Type: " << rec.content_type << std::endl;
+        std::cout << "Labels: " ;
+        for (auto& [key, value] : rec.labels) {
+            std::cout << key << ": " << value << ", ";
+        }
+        std::cout << std::endl;
+
+        // Read the content
+        auto [content, read_err] = rec.ReadAll();
+        assert(read_err == Error::kOk);
+
+        std::cout << "Content: " << content << std::endl;
+        assert(content == "Some binary data");
+
+        return true;    // if false, the query will stop
+    });
+
+    assert(err == Error::kOk);
+
+    return 0;
+}
