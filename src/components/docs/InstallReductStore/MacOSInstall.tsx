856e2118fabfe197206f5de2e4bff35479c86ec6
Alexey Timin <atimin@gmail.com>
2025-05-07 17:27:51 +0200

    update download page for new binaries

--- src/components/docs/InstallReductStore/MacOSInstall.tsx
+++ src/components/docs/InstallReductStore/MacOSInstall.tsx
@@ -1,17 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import CodeBlock from "@theme/CodeBlock";
+import styles from "./styles.module.css";
+import clsx from "clsx";
 
 export default function MacOSInstall(props) {
-  const binaryInstall = `
-wget https://github.com/reductstore/reductstore/releases/latest/download/reductstore.macos-amd64.tar.gz
-tar xfv reductstore.macos-amd64.tar.gz
+  const [activeTab, setActiveTab] = useState("binary (amd64)");
+
+  const binaryInstallAmd64 = `
+wget https://github.com/reductstore/reductstore/releases/latest/download/reductstore.x86_64-apple-darwin.tar.gz
+tar xfv reductstore.linux-amd64.tar.gz
 chmod +x reductstore
 RS_DATA_PATH=./data ./reductstore
 `.trim();
+
+  const binaryInstallArm64 = `
+wget https://github.com/reductstore/reductstore/releases/latest/download/reductstore.aarch64-apple-darwin.tar.gz
+tar xfv reductstore.linux-amd64.tar.gz
+chmod +x reductstore
+RS_DATA_PATH=./data ./reductstore
+`.trim();
+
   return (
     <>
-      <p>Compatible with the amd64 architecture.</p>
-      <CodeBlock className="language-bash">{binaryInstall}</CodeBlock>
+      <p>Compatible with the amd64 and arm64 architectures.</p>
+
+      <ul className={clsx("tabs", styles.tabs)}>
+        {["binary (amd64)", "binary (arm64)"].map((tab) => (
+          <li
+            key={tab}
+            className={clsx(
+              "tabs__item",
+              activeTab === tab && "tabs__item--active",
+            )}
+            onClick={() => setActiveTab(tab)}
+          >
+            {tab.charAt(0).toUpperCase() + tab.slice(1)}
+          </li>
+        ))}
+      </ul>
+
+      <div className={styles.tabContent}>
+        {activeTab === "binary (amd64)" && (
+          <CodeBlock className="language-bash">{binaryInstallAmd64}</CodeBlock>
+        )}
+        {activeTab === "binary (arm64)" && (
+          <CodeBlock className="language-bash">{binaryInstallArm64}</CodeBlock>
+        )}
+      </div>
     </>
   );
 }
