1d93338a740204ff9151cc5174fb24ebd17abb80
Anthony <anthony.cavin.ac@gmail.com>
2025-05-20 15:57:55 +0200

    KAN-152: Video tutorial ros2 image storage (#185)
    * rotate article date
    
    * add video and update code example
    
    * formatting
    
    * use compressed images
    
    * formatting
    
    * use ROS2 instead of ROs 2
    
    * update ROS distribution
    
    * update images to png
    
    * improve text

---
+++ src/components/shared/VideoPlayer/YouTube.tsx
@@ -0,0 +1,15 @@
+import styles from "./styles.module.css";
+
+export default function YouTube({ id }: { id: string }) {
+  return (
+    <div className={styles.videoWrapper}>
+      <iframe
+        className={styles.responsiveIframe}
+        src={`https://www.youtube.com/embed/${id}`}
+        title="YouTube Video Player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
+}
