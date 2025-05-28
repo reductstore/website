69fe4440a5efa60c422befbf592bc8bd329bba26
Alexey Timin <atimin@reduct.store>
2025-05-28 17:39:20 +0200

    update extension documentation for new staging (#199)

--- docs/examples/py/src/ext_select_query.py
+++ docs/examples/py/src/ext_select_query.py
@@ -11,6 +11,7 @@ async def main():
         # Write some CSV data with timestamps
         now = time_ns() // 1000
         await bucket.write("csv", "1,2,3,4,5\n6,7,8,9,10\n11,12,13,14,15\n", timestamp=now, content_type="text/csv")
+        await bucket.write("csv", "1,2,3,4,5\n6,7,8,9,10\n11,12,13,14,15\n", timestamp=now+1, content_type="text/csv")
 
         # Prepare the query with the 'select' extension
         ext = {
@@ -20,13 +21,16 @@ async def main():
                     {"index": 0, "as_label": "col1"},
                     # Select columns from 2nd to 4th column without labeling
                     {"from": 2, "to": 4},
-                ]
-            }
+                ],
+            },
+            "when": {
+                "@col1": {"$lt": 10},  # Filter out rows where the first column is greater than or equal to 10
+            },
         }
 
         # Query the data with the 'select' extension
         # and filter out records where the first column is less than 10
-        async for record in bucket.query("csv", start=now, when={"@col1": {"$lt": 10}}, ext=ext):
+        async for record in bucket.query("csv", start=now, ext=ext):
             print(f"Record timestamp: {record.timestamp}")
             print(f"Record labels: {record.labels}")
 
