269acd79b26eee8a5eace82e10d9c938cebac538
Alexey Timin <atimin@gmail.com>
2025-05-31 09:21:58 +0200

    finish quick start example

--- docs/examples/go/quick_start.go
+++ docs/examples/go/quick_start.go
@@ -20,7 +20,7 @@ func main() {
 		WithQuotaSize(1_000_000_000).
 		Build()
 
-	bucket, err := client.CreateOrGetBucket(ctx, "my-bucket", settings)
+	bucket, err := client.CreateOrGetBucket(ctx, "my-bucket", &settings)
 	if err != nil {
 		panic(err)
 	}
@@ -28,28 +28,28 @@ func main() {
 	// 3. Write some data with timestamps in the 'entry-1' entry
 	ts := time.Now().UnixMicro()
 	writer := bucket.BeginWrite(ctx, "entry-1", &reduct.WriteOptions{Timestamp: ts, Labels: map[string]any{"score": 10}})
-	err = writer.Write("<Blob data>", -1)
+	err = writer.Write("<Blob data>")
 	if err != nil {
 		panic(err)
 	}
 	writer = bucket.BeginWrite(ctx, "entry-1", &reduct.WriteOptions{Timestamp: ts + 1, Labels: map[string]any{"score": 20}})
-	err = writer.Write("<Blob data 2>", -1)
+	err = writer.Write("<Blob data 2>")
 	if err != nil {
 		panic(err)
 	}
 
 	// 4. Query the data by time range and condition
-	queryOptions := &reduct.QueryOptions{
-		Start: &ts,
-		When: map[string]any{
-			"&score": map[string]any{"$gt": 10},
-		},
-	}
+	queryOptions := reduct.NewQueryOptionsBuilder().
+		WithStart(ts).
+		WithStop(ts + 2).
+		WithWhen(map[string]any{"&score": map[string]any{"$gt": 15}}).
+		Build()
 
-	records, err := bucket.Query(ctx, "entry-1", queryOptions)
+	records, err := bucket.Query(ctx, "entry-1", &queryOptions)
 	if err != nil {
 		panic(err)
 	}
+
 	for rec := range records.Records() {
 		data, err := rec.Read()
 		if err != nil {
@@ -57,6 +57,8 @@ func main() {
 		}
 		timestamp := rec.Time()
 		labels := rec.Labels()
-		println("Record at time:", timestamp, "with labels:", labels, "and data:", string(data))
+		println("Record at time:", timestamp)
+		println("Labels:", labels)
+		println("Data:", string(data))
 	}
 }
