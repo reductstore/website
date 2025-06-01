9bd80a4b4b696467d263388f84d4e3575752cc65
Alexey Timin <atimin@gmail.com>
2025-06-01 11:17:08 +0200

    add snips and update ladning page

--- src/components/homepage/HomepageSDKs/index.tsx
+++ src/components/homepage/HomepageSDKs/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import CodeSnippetExample from "../../docs/CodeSnippetExample";
 import clsx from "clsx";
-import { SiPython, SiJavascript, SiRust, SiCplusplus } from "react-icons/si";
+import { SiPython, SiJavascript, SiRust, SiCplusplus, SiGo } from "react-icons/si";
 import styles from "./styles.module.css";
 import Link from "@docusaurus/Link";
 
@@ -32,6 +32,12 @@ export default function HomepageSDKs() {
               >
                 <SiJavascript size="3em" />
               </Link>
+                <Link
+                    className={styles.sdkIcon}
+                    to="/docs/getting-started/with-go"
+                >
+                  <SiGo size="3em" />
+                </Link>
               <Link
                 className={styles.sdkIcon}
                 to="/docs/getting-started/with-rust"
--- versioned_docs/version-1.15.x/examples/go/go.mod
+++ versioned_docs/version-1.15.x/examples/go/go.mod
@@ -2,4 +2,4 @@ module examples
 
 go 1.24.1
 
-require github.com/reductstore/reduct-go v0.0.4
+require github.com/reductstore/reduct-go v0.0.6
