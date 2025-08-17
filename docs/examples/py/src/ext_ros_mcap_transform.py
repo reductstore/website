5c5fd2587ab8c96fba2c1cf327b1b7e174fbb7ab
Anthony <anthony.cavin.ac@gmail.com>
2025-08-17 17:19:20 +0200

    example for MCAP transform extension

---
+++ docs/examples/py/src/ext_ros_mcap_transform.py
@@ -0,0 +1,51 @@
+from time import time_ns
+from pathlib import Path
+
+from reduct import Client
+
+HERE = Path(__file__).parent
+
+async def main():
+    async with Client("http://localhost:8383", api_token="my-token") as client:
+        bucket = await client.create_bucket(
+            "my-bucket",
+            exist_ok=True,
+        )
+        # Write an MCAP file with timestamps
+        now = time_ns() // 1000
+        with open(f"{HERE}/../data/file.mcap", "rb") as f:
+            data = f.read()
+
+        await bucket.write(
+            "mcap",
+            data,
+            content_length=len(data),
+            timestamp=now,
+            content_type="application/mcap",
+        )
+
+        # Prepare the query with the 'ros' extension (transform)
+        ext = {
+            "ros": {
+                "transform": {
+                    "split": {"duration": "1m", "size": "100MB"},
+                    "topics": {
+                        "include": ["topic-1", "/topic-.*/"],
+                        "exclude": ["topic-b", "/ext-.*/"],
+                    },
+                }
+            }
+        }
+
+        # Query the data with the 'ros' extension
+        async for record in bucket.query("mcap", start=now, ext=ext):
+            print(f"Record timestamp: {record.timestamp}")
+
+            # Each record corresponds to a new MCAP episode
+            data = await record.read_all()
+            print(f"Episode file size: {len(data)} bytes")
+
+
+if __name__ == "__main__":
+    import asyncio
+    asyncio.run(main())
