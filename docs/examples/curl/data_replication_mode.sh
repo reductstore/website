ee6fe6fe34589278ae6232c0800f0e27a4ec8154
Alexey Timin <atimin@gmail.com>
2025-12-19 17:12:15 +0100

    fix copilot comments

--- docs/examples/curl/data_replication_mode.sh
+++ docs/examples/curl/data_replication_mode.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e -x
+set -e
 
 API_PATH="http://127.0.0.1:8383/api/v1"
 AUTH_HEADER="Authorization: Bearer my-token"
@@ -8,16 +8,16 @@ AUTH_HEADER="Authorization: Bearer my-token"
 curl -X PATCH \
   -d '{"mode":"paused"}' \
   -H "${AUTH_HEADER}" \
-  -a "${API_PATH}"/replications/my_replication/mode
+  "${API_PATH}"/replications/my_replication/mode
 
 # Disable replication to ignore new transactions
 curl -X PATCH \
   -d '{"mode":"disabled"}' \
   -H "${AUTH_HEADER}" \
-  -a "${API_PATH}"/replications/my_replication/mode
+  "${API_PATH}"/replications/my_replication/mode
 
 # Switch back to enabled after maintenance to resume sending (and flush any queued transactions)
 curl -X PATCH \
   -d '{"mode":"enabled"}' \
   -H "${AUTH_HEADER}" \
-  -a "${API_PATH}"/replications/my_replication/mode
+  "${API_PATH}"/replications/my_replication/mode
