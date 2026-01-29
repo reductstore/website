2978ead632b00616d984bc1e7f642e69212d43de
Anthony <anthony.cavin.ac@gmail.com>
2026-01-29 12:09:22 +0100

    update blog images

--- src/data/useCasesData.ts
+++ src/data/useCasesData.ts
@@ -20,8 +20,9 @@ const useCases: UseCase[] = [
     description:
       "Learn how to store and manage data for edge computing and AI application in manufacturing.",
     link: "/blog/daq-manufacture-system",
-    image: require("@site/blog/2025-03-17-daq-manufacture-system/img/daq.png")
-      .default,
+    image:
+      require("@site/blog/2025-03-17-daq-manufacture-system/img/daq-intro.png")
+        .default,
   },
   {
     title: "Computer Vision",
@@ -29,7 +30,7 @@ const useCases: UseCase[] = [
       "Explore how to implement computer vision applications in industrial settings with practical examples.",
     link: "/blog/computer-vision-applications",
     image:
-      require("@site/blog/2025-07-08-3-ways-store-data-for-computer-vision-applications/img/reductStore-usage.png")
+      require("@site/blog/2025-07-08-3-ways-store-data-for-computer-vision-applications/img/cv-intro.png")
         .default,
   },
   {
@@ -38,7 +39,7 @@ const useCases: UseCase[] = [
       "Strategies for reducing and storing vibration sensor data effectively.",
     link: "/blog/how-to-store-vibration-sensor-data",
     image:
-      require("@site/blog/2024-12-05-data-flow-with-vibration-data/img/vibration_data_flow_intro.webp")
+      require("@site/blog/2024-12-05-data-flow-with-vibration-data/img/vibration_data_flow_intro.png")
         .default,
   },
   {
@@ -47,7 +48,7 @@ const useCases: UseCase[] = [
       "Best practices for storing and managing MQTT data in IIoT applications.",
     link: "/blog/advice/database/mqtt-data-storage",
     image:
-      require("@site/blog/2025-05-08-mqtt-data-storage/img/social-card.jpg")
+      require("@site/blog/2025-05-08-mqtt-data-storage/img/mqtt-data-storage.png")
         .default,
   },
   {
@@ -56,7 +57,7 @@ const useCases: UseCase[] = [
       "Learn how to set up a data sink using Apache Kafka for data streaming applications.",
     link: "/blog/tutorial/datastreaming/kafka/data-sink-guide",
     image:
-      require("@site/blog/2024-02-04-kafka-tutorial-data-sink/img/kafka-reductstore.webp")
+      require("@site/blog/2024-02-04-kafka-tutorial-data-sink/img/kafka-reductstore.png")
         .default,
   },
   {
@@ -65,7 +66,7 @@ const useCases: UseCase[] = [
       "Implement open-source AI anomaly detection at the edge with practical examples.",
     link: "/blog/computer-vision/edge-computing/ai/Implementing-open-source-ai-anomaly-detection",
     image:
-      require("@site/blog/2023-10-15-Implementing-open-source-ai-anomaly-detection/img/randy-fath-chess.webp")
+      require("@site/blog/2023-10-15-Implementing-open-source-ai-anomaly-detection/img/anomaly_detection_banner.png")
         .default,
   },
   {
@@ -74,7 +75,7 @@ const useCases: UseCase[] = [
       "Techniques for streaming database data into PyTorch for machine learning applications.",
     link: "/blog/ai/datastreaming/pytorch/implement-database-data-streaming-pytorch",
     image:
-      require("@site/blog/2024-01-13-implement-data-streaming-pytorch/img/pytorch-iterabledataset.webp")
+      require("@site/blog/2024-01-13-implement-data-streaming-pytorch/img/pytorch-iterabledataset.png")
         .default,
   },
 ];
