7d043cafa56f2513eacdfaf511a2af67ad5f820e
Alexey Timin <atimin@gmail.com>
2026-04-07 15:34:55 +0200

    docs(js): use string replication modes in examples

--- versioned_docs/version-1.18.x/examples/js/src/data_replication_mode.mjs
+++ versioned_docs/version-1.18.x/examples/js/src/data_replication_mode.mjs
@@ -1,16 +1,16 @@
 // reduct-js is published as CommonJS; in Node ESM examples we import the default export.
 import reductJs from "reduct-js";
 
-const { Client, ReplicationMode } = reductJs;
+const { Client } = reductJs;
 
 // Update replication mode without changing other settings
 const client = new Client("http://127.0.0.1:8383", { apiToken: "my-token" });
 
 // Pause to queue transactions without sending them
-await client.setReplicationMode("my_replication", ReplicationMode.PAUSED);
+await client.setReplicationMode("my_replication", "paused");
 
 // Disable to ignore new transactions
-await client.setReplicationMode("my_replication", ReplicationMode.DISABLED);
+await client.setReplicationMode("my_replication", "disabled");
 
 // Re-enable to resume sending queued data
-await client.setReplicationMode("my_replication", ReplicationMode.ENABLED);
+await client.setReplicationMode("my_replication", "enabled");
