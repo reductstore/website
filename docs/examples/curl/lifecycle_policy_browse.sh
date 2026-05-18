2cb904f11094a081fcdb4dac3e45d1687fbbe183
Anthony <26444489+AnthonyCvn@users.noreply.github.com>
2026-05-18 18:26:16 +0200

    add curl example

---
+++ docs/examples/curl/lifecycle_policy_browse.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e -x
+
+API_PATH="http://127.0.0.1:8383/api/v1"
+AUTH_HEADER="Authorization: Bearer my-token"
+
+curl -H "${AUTH_HEADER}" \
+  "${API_PATH}"/lifecycles
+
+curl -H "${AUTH_HEADER}" \
+  "${API_PATH}"/lifecycles/my-lifecycle
