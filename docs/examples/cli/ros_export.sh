aae25aabecd09ab620409c244e16be3ba6ba40c2
Alexey Timin <atimin@gmail.com>
2026-08-21 14:06:28 +0200

    add limits

--- docs/examples/cli/ros_export.sh
+++ docs/examples/cli/ros_export.sh
@@ -11,7 +11,8 @@ reduct-cli cp \
   https://reductstore@play.reduct.store/replica/orion \
   ./orion-native \
   --entries 'right_ir/rotated/*' \
-  --start 'now-1h'
+  --start 'now-1h' \
+  --limit 1
 # ros-native-export-end
 
 native_file=$(find ./orion-native -type f -print -quit)
@@ -24,6 +25,7 @@ reduct-cli cp \
   ./orion-mcap \
   --entries 'right_ir/rotated/image_raw' \
   --start 'now-1h' \
+  --limit 1 \
   --when '{"#ext":{"ros":{"export":{"format":"mcap"}}}}'
 # ros-mcap-export-end
 
